0620315a852d6bf51864396c75768e9e90dd695d
Axel Naumann <Axel.Naumann@cern.ch>
2020-05-18 11:11:30 +0200

    Do not run notification if unconfigured.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
+if [[ "x$INPUT_PASSWORD" == "x" ]] ; then
+    echo "debug file={entrypoint.sh}:: No 'INPUT_PASSWORD' parameter; assuming this is a fork for which no email notification is configured. Exiting gracefully."
+    exit
+fi
+
 pip3 install github3.py unidiff
 python3 /entrypoint.py
