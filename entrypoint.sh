104f9caf4ac94465b6c5f123027a92347fec180f
Axel Naumann <Axel.Naumann@cern.ch>
2020-05-18 11:40:12 +0200

    Add a helpful message.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if test "x$INPUT_GITHUBTOKEN" = "x" ; then
     echo "No 'INPUT_GITHUBTOKEN' parameter; assuming this is a fork for which no email notification is configured. Exiting gracefully."
+    echo "Consider disabling Actions for ROOT at Github / ${GITHUB_REPOSITORY} / Settings / Actions / Disable Actions for this repository."
     exit
 fi
 
